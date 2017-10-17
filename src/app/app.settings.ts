0bda52a27b60dcaafb324ae033c2deab169682fd
David <debeltranc@unal.edu.co>
2017-10-17 07:14:48 +0000

    chore: header and footer

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -3,6 +3,6 @@ import { Headers } from "@angular/http";
 
 @Injectable()
 export class AppGlobals {
-  public static APIURI: string = "http://192.168.99.101:3000";
+  public static APIURI: string = "http://192.168.99.101:6000";
   public static URIHEADERS: Headers = new Headers( { "Content-Type": "application/json", "Accept": "application/json" } )
 }
