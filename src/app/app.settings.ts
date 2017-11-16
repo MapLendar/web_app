1edf9118ddaf1ecde2e19705838235baa6515fd8
David <debeltranc@unal.edu.co>
2017-11-16 18:56:21 +0000

    chore: various changes

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -3,7 +3,7 @@ import { Headers } from "@angular/http";
 
 @Injectable()
 export class AppGlobals {
-  public static APIURI: string = "http://192.168.99.101:6000";
+  public static APIURI: string = "http://35.196.224.24:3000";
   public URIHEADERS: Headers = new Headers( { "Content-Type": "application/json", "Accept": "application/json" } )
   
   public isLoggedIn(): boolean{
